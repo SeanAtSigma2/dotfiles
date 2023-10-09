eb2a0e21e5f01fc31f024dff7df2e38ba42c1a0a
Sean Lawrence <sean@sigma2.io>
2023-10-09 14:50:07 -0500

    add mutt configuration for email
    Assumes there is a 1password account, CLI and entry for mutt

--- pull_latest.sh
+++ pull_latest.sh
@@ -2,11 +2,20 @@
 
 set -e
 
+# TMUX
 cp ~/.tmux.conf ./.tmux.conf
+
+# ZSH
 cp ~/.zshrc ./.zshrc
+
+# Neovim
 cp -R ~/.config/nvim/lua ./nvim/
 cp ~/.config/nvim/.neoconf.json ./nvim/.neoconf.json
 cp ~/.config/nvim/stylua.toml ./nvim/stylua.toml
 cp ~/.config/nvim/init.lua ./nvim/init.lua
 cp ~/.config/nvim/lazy-lock.json ./nvim/lazy-lock.json
 rm -rf ./nvim/.git
+
+# Mutt
+cp ~/.mutt/muttrc ./.mutt/muttrc
+cp ~/.mutt/mailcap ./.mutt/mailcap
